4271d398d9ca0bc042efdf8463496da118c5d053
Louis Finegan <118827082+Louis-Finegan@users.noreply.github.com>
2022-12-15 13:29:21 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 # initialize the package
-from curvefit import curve_fit
-from curvefit import curve_fit_guess_limit
-from curvefit import curve_fit_limit
+from .curvefit import curve_fit
+from .curvefit import curve_fit_guess_limit
+from .curvefit import curve_fit_limit
